d7f11867fc3348f3bcca2f7dd0893d8170e0c544
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-03-04 15:52:03 +0600

    modal complete

--- Js/app.js
+++ Js/app.js
@@ -72,7 +72,65 @@ const displayDetails = (detail) => {
   modalTitle.innerText = detail.tool_name;
   const bodyDetail = document.getElementById("detail-body");
   bodyDetail.innerHTML = `
-  
+  <div class="row row-cols-1 row-cols-md-2 g-4">  
+  <div class="col">
+    <div class="card m-3 bg-danger-subtle p-3">      
+      <div class="card-body">
+        <h6 class="card-title">${detail.description}</h6>
+        <div class="d-flex justify-content-between">
+           <div class="bg-info-subtle border border-0 border-info rounded m-3 w-25 h-25">
+             <p class="p-2 text-center text-success fs-6">
+             ${detail.pricing[0].price}
+             <br>
+             ${detail.pricing[0].plan}             
+             </p>             
+           </div>
+           <div class="bg-info-subtle border border-0 border-info rounded m-3 w-25 h-25">
+             <p class="p-2 text-center text-primary fs-6">
+             ${detail.pricing[1].price}
+             <br>
+             ${detail.pricing[1].plan}             
+             </p>             
+           </div>
+           <div class="bg-info-subtle border border-0 border-info rounded m-3 w-25 h-25">
+             <p class="p-2 text-center text-danger fs-6">
+             ${detail.pricing[2].price}
+             <br>
+             ${detail.pricing[2].plan}             
+             </p>             
+           </div>
+        </div>
+        <div class="d-flex justify-content-around">
+            <div>
+                <h5 class="fw-bold text-info-emphasis">Features</h5>
+                   <ul>
+                     <li>${detail.features[1].feature_name}</li>
+                     <li>${detail.features[2].feature_name}</li>
+                     <li>${detail.features[3].feature_name}</li>
+                   </ul>
+            </div>
+            <div>
+                <h5 class="fw-bold text-info-emphasis">Integrations</h5>
+                   <ul>
+                       <li>${detail.integrations[0]}</li>
+                       <li>${detail.integrations[1]}</li>
+                       <li>${detail.integrations[2]}</li>
+                  </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card m-3 p-3">
+      <img src="${detail.image_link[0]}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-center fst-italic fw-bolder m-3">${detail.input_output_examples[0].input}</h5>
+        <p class="card-text text-center">${detail.input_output_examples[0].output}.</p>
+      </div>
+    </div>
+  </div>
+</div>
   `;
 };
 
